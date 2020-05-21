5db701acf5598c619f0b70ca3ce1ed3e18a52e17
MikaelUrankar <mikael.urankar@gmail.com>
2020-05-21 18:33:59 +0200

    Connect the uname plugin
    zabbix_agent2 -t system.uname
    zabbix_agent2 -t system.hostname
    zabbix_agent2 -t system.sw.arch

--- zabbix50-agent2/files/patch-src_go_plugins_plugins__freebsd.go
+++ zabbix50-agent2/files/patch-src_go_plugins_plugins__freebsd.go
@@ -1,6 +1,6 @@
 --- plugins_freebsd.go.orig	2020-05-21 11:42:53.409675000 +0200
 +++ plugins_freebsd.go	2020-05-21 11:46:17.986388000 +0200
-@@ -0,0 +1,34 @@
+@@ -0,0 +1,35 @@
 +/*
 +** Zabbix
 +** Copyright (C) 2001-2020 Zabbix SIA
@@ -30,6 +30,7 @@
 +	_ "zabbix.com/plugins/net/netif"
 +	_ "zabbix.com/plugins/redis"
 +	_ "zabbix.com/plugins/systemrun"
++	_ "zabbix.com/plugins/system/uname"
 +	_ "zabbix.com/plugins/web"
 +	_ "zabbix.com/plugins/zabbix/async"
 +	_ "zabbix.com/plugins/zabbix/stats"
